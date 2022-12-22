a9c2fa20a055c263d770fb9fae6ad1ea7c4fa5aa
ManirMohammadManiruzzaman <manir121234@gmail.com>
2022-12-22 12:12:11 +0600

    2nd task

---
+++ 2ndjs.js
@@ -0,0 +1,8 @@
+function userpass(u,p){
+document.write("your user name is: "+u);
+document.write("your user name is: "+p);
+
+}
+let a=prompt(" enter your user name please:");
+let b=prompt("enter your user name please:");
+userpass(a,b);
